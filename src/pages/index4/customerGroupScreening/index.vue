60d8b8002146c500ba48a5317082fe075a5d9683
longjianwen0826 <1073512789@qq.com>
2019-10-07 17:46:13 +0800

    上传

--- src/pages/index4/customerGroupScreening/index.vue
+++ src/pages/index4/customerGroupScreening/index.vue
@@ -1,32 +1,85 @@
 <template>
-
   <div class="main">
-
     <!-- <van-nav-bar
       :title='title'
       fixed
-    /> -->
+    />-->
     <div class="menu-sc">
-      <van-search placeholder="请输入客户名称" class="vv-search"/>
-      <van-button style="margin:5px;" size="small" type="default">搜索</van-button>
-      <van-icon name="location" />思明区
+      <van-search style="height:25px;" placeholder="请输入客户名称" class="vv-search" />
+      <van-button class="vbtn-small" size="small" type="default">搜索</van-button>
+      <img class="dy-icon" src="../../../assets/images/huidiantong/1_0001s_0003.png" alt />
+      <span class="span-dw">思明区</span>
     </div>
+    <div class="region-module" v-show="value1==0">
+      <span class="region-modulespan" style="margin-right:20px;">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0009.png" alt />
+        <p class="module-p">我行</p>
+      </span>
+      <span class="region-modulespan">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0008.png" alt />
+        <p class="module-p">非我行</p>
+      </span>
+    </div>
+
+    <div class="region-module" v-show="value1==1">
+      <span class="region-modulespan" style="margin-right:8px;">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0006.png" alt />
+        <p class="module-p">强</p>
+      </span>
+      <span class="region-modulespan" style="margin-right:8px;">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0004.png" alt />
+        <p class="module-p">中</p>
+      </span>
+      <span class="region-modulespan" style="margin-right:8px;">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0007.png" alt />
+        <p class="module-p">弱</p>
+      </span>
+      <span class="region-modulespan">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0005.png" alt />
+        <p class="module-p">无数据</p>
+      </span>
+    </div>
+
+    <div class="region-module" style="padding:0px 3px;" v-show="value1==2">
+      <span class="region-modulespan" style="margin-right:3px;">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0006.png" alt />
+        <p class="module-p">优质</p>
+      </span>
+      <span class="region-modulespan" style="margin-right:3px;">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0004.png" alt />
+        <p class="module-p">良好</p>
+      </span>
+      <span class="region-modulespan" style="margin-right:3px;">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0007.png" alt />
+        <p class="module-p">一般</p>
+      </span>
+      <span class="region-modulespan">
+        <img class="region-module-icon" src="../../../assets/images/huidiantong/1_0001s_0005.png" alt />
+        <p class="module-p">无数据</p>
+      </span>
+    </div>
+
+    <div class="region-slectModule">
+      <van-dropdown-menu id="slectModule" :overlay="false" style="height:30px;">
+        <van-dropdown-item style="height:170px;" v-model="value1" @change="chaVal" :options="option1" />
+      </van-dropdown-menu>
+    </div>
+
     <div class="content">
-      <!-- <div class="msg">惠点通</div> -->
       <baidu-map id="container" :mapClick="false" :center="center" :zoom="zoom" @ready="handler">
         <div v-for="(marker, i) of markers" :key="i">
           <bm-marker :position="{lng: subSSS(marker.center,0), lat: subSSS(marker.center,1)}" @click="infoWindowOpen(i)" :dragging="true" animation="BMAP_ANIMATION_BOUNCE">
-            <bm-info-window :title="marker.pname" :show="marker.showFlag" @close="infoWindowClose(i)" @open="infoWindowOpen(i)">
-              <div class="fosize">项目名称：{{marker.pname}}</div>
-              <div class="fosize">设备数量：{{marker.devices}}</div>
+            <bm-info-window :width="60" :title="marker.pname" :show="marker.showFlag" @close="infoWindowClose(i)" @open="infoWindowOpen(i)">
+              <div class="fosize">客户需求：中</div>
+              <div class="fosize">客户分层：一般</div>
             </bm-info-window>
           </bm-marker>
         </div>
-        <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+        <!-- <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation> -->
       </baidu-map>
     </div>
     <!--底部导航-->
-    <BottomBar/>
+    <BottomBar />
     <!--覆盖底部导航-->
     <div class="botBarFuGaiDiv">
       <van-row >
@@ -143,92 +196,133 @@
         </van-col>
       </van-row>
     </van-action-sheet>
-
   </div>
-
 </template>
-
 <script>
 //引入组件首字母大写
-import BottomBar from '@/components/bottomBar'
+import BottomBar from "@/components/bottomBar";
+import iconCar from '../../../assets/images/huidiantong/1_0001s_0003.png'; //以import的方式导入图片文件
 export default {
-
   //基础数据存放处
-  data (){
-     return {
-       title : '惠点通',
-       //修改地图底色
-        mapStyle: {
-         style : "midnight"
+  data() {
+    return {
+      //切换模式
+      value1: 0,
+      option1: [
+        { text: '客户拓展模式', value: 0 },
+        { text: '客户需求模式', value: 1 },
+        { text: '客户分层模式', value: 2 }
+      ],
+
+      title: "惠点通",
+      //修改地图底色
+      mapStyle: {
+        style: "midnight"
+      },
+      //聚合列表 项目名称坐标设备数量
+      markers: [
+        {
+          center: "113.27147,23.131669",
+          showFlag: false,
+          pname: "厦门连平方科技有限公司",
+          pSbNum: 3
         },
-        //聚合列表 项目名称坐标设备数量
-        markers:[
-          {center:'113.27147,23.131669',showFlag:false,pname:'消防火灾监控系统',pSbNum:3},
-          {center:'113.263423,23.131963',showFlag:false,pname:'消防火灾监控系统2',pSbNum:4},
-          {center:'113.298996,23.099108',showFlag:false,pname:'消防火灾监控系统3',pSbNum:5},
-          {center:'113.241863,23.091626',showFlag:false,pname:'消防火灾监控系统4',pSbNum:7},
-          {center:'113.214159,23.074407',showFlag:false,pname:'消防火灾监控系统5',pSbNum:9}
-        ],
-        center: {lng: 113.27147, lat: 23.131669},
-        zoom: 3,
-        show: true,
-        //惠点通上拉菜单使用
-        huiShow:true,
-        customerType: [],//客群类型选中
-        customerTypeS: [//客群类型数据
-          { name: '核心客群', value: 1 },
-          { name: '主力客群', value: 2 },
-          { name: '大众客群', value: 3 },
-        ],
-        platform: [],//客群平台选中
-        platformS: [//客群平台数据
-          { name: '商业聚类', value: 1 },
-          { name: '政府聚类', value: 2 },
-          { name: '内部聚类', value: 3 },
-          { name: '数据驱动聚类', value: 4 },
-        ],
-        clientId: null,//小微企业客户群编号
-        clientName: '',//小微企业客户群名称
-        tagId: null,//标签id
-        tagName: '',//标签名称
-        clientHierarchy: '',//客群层级
-        tagHierarchy: '',//标签层级
-     }
+        {
+          center: "113.263423,23.131963",
+          showFlag: false,
+          pname: "厦门连平方科技有限公司",
+          pSbNum: 4
+        },
+        {
+          center: "113.298996,23.099108",
+          showFlag: false,
+          pname: "厦门连平方科技有限公司",
+          pSbNum: 5
+        },
+        {
+          center: "113.241863,23.091626",
+          showFlag: false,
+          pname: "厦门连平方科技有限公司",
+          pSbNum: 7
+        },
+        {
+          center: "113.214159,23.074407",
+          showFlag: false,
+          pname: "厦门连平方科技有限公司",
+          pSbNum: 9
+        }
+      ],
+      center: { lng: 113.27147, lat: 23.131669 },
+      zoom: 3,
+      show: true,
+      //惠点通上拉菜单使用
+      huiShow:true,
+      customerType: [],//客群类型选中
+      customerTypeS: [//客群类型数据
+        { name: '核心客群', value: 1 },
+        { name: '主力客群', value: 2 },
+        { name: '大众客群', value: 3 },
+      ],
+      platform: [],//客群平台选中
+      platformS: [//客群平台数据
+        { name: '商业聚类', value: 1 },
+        { name: '政府聚类', value: 2 },
+        { name: '内部聚类', value: 3 },
+        { name: '数据驱动聚类', value: 4 },
+      ],
+      clientId: null,//小微企业客户群编号
+      clientName: '',//小微企业客户群名称
+      tagId: null,//标签id
+      tagName: '',//标签名称
+      clientHierarchy: '',//客群层级
+      tagHierarchy: '',//标签层级
+    };
   },
 
   //数据预加载
-  created : function(){
-
-  },
+  created: function() {},
 
   //网页加载完成
-  mounted : function(){
+  mounted: function() {
+    /* const map = new BMap.Map("container");
+    const point = new BMap.Point(113.27147, 23.131669);
+    map.centerAndZoom(point, 15);
+    map.setCurrentCity("guangzhou");
+    const deviceSize = new BMap.Size(48,48);
+    const deviceIcon = new BMap.Icon(iconCar, deviceSize, { //会以base64的方式传参iconCar
+        imageSize: deviceSize
+    });
+    const marker = new BMap.Marker(point, {
+        icon: deviceIcon
+    }); */
 
   },
 
   //声明方法
-  methods : {
-
-    go : function(){
-      this.$router.push('/more');
+  methods: {
+    chaVal(){
+      console.log(this.value1)
+    },
+    go: function() {
+      this.$router.push("/more");
     },
 
-    handler ({BMap, map}) {
+    handler({ BMap, map }) {
       //默认经纬度
-      this.center.lng = 113.27147
-      this.center.lat = 23.131669
-      this.zoom = 13
+      this.center.lng = 113.27147;
+      this.center.lat = 23.131669;
+      this.zoom = 18;
     },
     //坐标截取
-    subSSS(row,xy){
+    subSSS(row, xy) {
       var indexRow = row.split(",");
-      return indexRow[xy]
+      return indexRow[xy];
     },
-    infoWindowClose (marker) {
-      this.markers[marker].showFlag = false
+    infoWindowClose(marker) {
+      this.markers[marker].showFlag = false;
     },
-    infoWindowOpen (marker) {
-      this.markers[marker].showFlag = true
+    infoWindowOpen(marker) {
+      this.markers[marker].showFlag = true;
     },
     //点击打开客群筛选上拉菜单
     opencustomer(){
@@ -245,20 +339,15 @@ export default {
       this.clientHierarchy= '';//客群层级
       this.tagHierarchy= '';//标签层级
     },
-
-
   },
 
   //计算属性
-  computed: {
-
-  },
-
+  computed: {},
 
   components: {
     BottomBar
   }
-}
+};
 </script>
 
 <style>
@@ -284,35 +373,106 @@ export default {
   }
 </style>
 <style lang="scss" scoped>
-
-.menu-sc{
- position:fixed;
- top:0px;
- left:0px;
- width:100%;
- height:46px;
- line-height:46px;
- text-align:center;
- background-color:#fff;
- z-index:1;
+.region-modulespan{
+  float: left;
 }
-#near{
+.region-module-icon{
+  width: 11px;
+  float: left;
+  margin: 8px 5px;
+}
+.dy-icon {
+  position: relative;
+  top: 3px;
+  width: 13px;
+  margin-right: 3px;
+}
+.span-dw {
+  color: #ffffff;
+  font-size: 15px;
+}
+.van-cell {
+  line-height: 28px !important;
+}
+.vbtn-small {
+  margin: 10px 5px 0px 0px;
+  border-radius: 3px;
+  height: 28px;
+  line-height: 28px;
+}
+.van-search__content {
+  height: 28px;
+  line-height: 28px;
+}
+.van-search .van-cell {
+  padding: 0px 8px 0px 0px;
+}
+.menu-sc {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 46px;
+  line-height: 46px;
+  text-align: center;
+  background-color: #379bf6;
+  z-index: 1;
+}
+.region-module{
+  position: fixed;
+  top: 58px;
+  left: 10px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  background-color: #ffffff;
+  border:solid 0.5px #eeeeee;  
+  border-radius: 8px;
+  z-index: 1;
+  padding:0px 15px 0px 10px;
+}
+.module-p{
+  float:left;
+  margin: 0;
+  font-size: 14px;
+  color: #333;
+}
+.region-slectModule{
+  position: fixed;
+  top: 58px;
+  right: 10px;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  background-color: #ffffff;
+  border:solid 0.5px #eeeeee;  
+  border-radius: 8px;
+  z-index: 1;
+  padding:0px 10px 0px 8px;
+}
+#slectModule .van-dropdown-item--down {
+    width: 170px !important;
+}
+#near {
   width: 100%;
   height: 100%;
   position: absolute;
 }
-#container{
-  width:100%;
-  height:100%;
-  height:736px;
-  float:left;
-}
-.vv-search{
-  width: auto;
-  min-width: 63%;
+#container {
+  width: 100%;
+  height: 100%;
+  height: 736px;
   float: left;
-  line-height: 46px;
-  height: 46px;
+}
+.vv-search {
+  width: auto;
+  min-width: 60%;
+  float: left;
+  line-height: 28px;
+  height: 28px;
+  margin: 11px 0px 0px 12px;
+  padding: 0px;
+  border-radius: 15px;
 }
  //惠点通上拉菜单
   .hui_Action_ke{
